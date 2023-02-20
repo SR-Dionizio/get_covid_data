45c841d3efc82510934469818e9bdfd3bc2bff17
Daniel Dionizio <daniel_santos300@hotmail.com>
2023-02-20 13:32:37 -0300

    adicionando informações do site

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import requests
 from bs4 import beautifulsoup
 
+url = 'https://infoms.saude.gov.br/extensions/covid-19_html/covid-19_html.html'
+headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+
+        
