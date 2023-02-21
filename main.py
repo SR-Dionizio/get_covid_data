940e6c73aca5d77ffa6d7893d204ab20c737fd11
Daniel Dionizio <daniel_santos300@hotmail.com>
2023-02-21 12:42:40 -0300

    fazendo raspagem do total de casos

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import re
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url = 'https://news.google.com/covid19/map?hl=pt-BR&mid=%2Fm%2F015fr&gl=BR&ceid=BR%3Apt-419&state=1'
 
@@ -10,8 +12,19 @@ soup = BeautifulSoup(html, 'html.parser')
 
 estatistica_covid19 = soup.find('table', class_='pH8O4c')
 por_estados = estatistica_covid19.find_all('th', class_= 'l3HOY')
+total_casos = estatistica_covid19.find_all('tr')
 
-for estatistica in por_estados:
-    estados = estatistica.find('div', class_ = 'pcAJd').get_text()
+covid19 = []
 
-    print(estados)
+for estatistica1, estatistica2 in zip(por_estados, total_casos):
+    estatistica1 = estatistica1.get_text()
+    covid19.append(estatistica1)
+
+
+
+    casos = estatistica2.find('td')
+    if casos is not None:
+        texto_formatado = casos.string
+        if texto_formatado:
+            covid19.append(texto_formatado)
+print(covid19)
