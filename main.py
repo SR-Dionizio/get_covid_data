2049db38140ce0746b41d0d44ab372bc69a5cc97
Daniel Dionizio <daniel_santos300@hotmail.com>
2023-02-21 23:27:35 -0300

    criando DataFrame e adicionando em csv

--- main.py
+++ main.py
@@ -14,17 +14,30 @@ tabela_covid19 = soup.find('table', class_='pH8O4c')
 
 linhas = tabela_covid19.find_all('tr')
 
+lista_estados = []
+lista_casos = []
+lista_mortes = []
 
 for linha in linhas:
     estados = linha.find('th').get_text()
+    lista_estados.append(estados)
+    
+
     
     casos = linha.find('td')
     if casos is not None:
         casos_formatado = casos.string
         if casos_formatado:
-            pass
+            lista_casos.append(casos_formatado)
 
     mortes = linha.find_all('td')
     if len(mortes) >= 5:
         mortes_formatado = mortes[4]
-        print(mortes_formatado.get_text())
+        lista_mortes.append(mortes_formatado.text)
+
+lista_estados.pop(0)
+df = pd.DataFrame({'Estados': lista_estados, 'Casos': lista_casos, 'Mortes': lista_mortes})
+print(df)
+
+df.to_csv('covid19.csv', mode='a', header=True, index=False, sep=';')
+
